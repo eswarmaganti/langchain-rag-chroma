059cf8dcb3d5070d545943f8031a1a05701c1110
Eswar Krishna Maganti <maganti.ek@gmail.com>
2025-05-27 06:12:29 +0530

    reformatted the main.py file

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ Answer the question based on the above context: {question}
 def main():
 
     # parsing the commandline arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("query_text", type=str, help="The query text,")
-    args = parser.parse_args()
-    query_text = args.query_text
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("query_text", type=str, help="The query text,")
+    # args = parser.parse_args()
+    # query_text = args.query_text
+
 
     # initializing the embeddings model and database
     db = Chroma(
@@ -31,29 +32,40 @@ def main():
         embedding_function=OllamaEmbeddings(model="mxbai-embed-large")
     )
 
-    # search the chroma db to find the similar results
-    results = db.similarity_search_with_relevance_scores(query_text, k=3)
-    if len(results) == 0:
-        # print(results)
-        # print("\n\n")
-        print("Unable to find matching results")
-        return
+    while True:
+        # reading the user input
+        query_text = input("Ask your question (q for quit): ")
 
-    # preparing the prompt
-    context_text = "\n\n---\n\n".join([ doc.page_content for doc, _score in results ])
-    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    prompt = prompt_template.format(context = context_text, question=query_text)
-    print(prompt)
+        if query_text in ["q" , 'Q']:
+            print("------------------- Exiting Bye :bye ---------------")
+            return
 
-    #  initializing the model to predict the response
-    model = ChatOllama(model="llama3.2")
-    response_text = model.invoke(prompt)
+        # search the chroma db to find the similar results
+        results = db.similarity_search_with_relevance_scores(query_text, k=3)
+        if len(results) == 0:
+            # print(results)
+            # print("\n\n")
+            print("Unable to find matching results")
+            return
 
-    # preparing the result sources from document metadata
-    sources = [doc.metadata.get("source", None) for doc, _score in results]
-    formatted_response = f"Response: {response_text.content}\nSources: {sources}"
+        # preparing the prompt
+        context_text = "\n---\n".join([ doc.page_content for doc, _score in results ])
+        prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+        prompt = prompt_template.format(context = context_text, question=query_text)
+        print("----------------------- PROMPT --------------------")
+        print(prompt)
+        print("----------------------- PROMPT --------------------")
 
-    print(formatted_response)
+        #  initializing the model to predict the response
+        model = ChatOllama(model="llama3.2")
+        response_text = model.invoke(prompt)
+
+        # preparing the result sources from document metadata
+        sources = [doc.metadata.get("source", None) for doc, _score in results]
+        formatted_response = f"{response_text.content}\nSources: {sources}"
+        print("\n----------------------- RESPONSE --------------------")
+        print(formatted_response)
+        print("----------------------- RESPONSE --------------------\n\n")
 
 
 # main starts here
